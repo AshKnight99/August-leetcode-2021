5801fb429cda1276a54fadf8b0fad2e5273d6bcf
AshKnight99 <rachanaraut1999@gmail.com>
2021-08-28 21:24:19 +0530

    Day 28 commit

---
+++ Day 28.java
@@ -0,0 +1,71 @@
+/*
+Maximum Profit in Job Scheduling
+We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
+
+You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there are no two jobs in the subset with overlapping time range.
+
+If you choose a job that ends at time X you will be able to start another job that starts at time X.
+
+ 
+
+Example 1:
+
+
+
+Input: startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70]
+Output: 120
+Explanation: The subset chosen is the first and fourth job. 
+Time range [1-3]+[3-6] , we get profit of 120 = 50 + 70.
+Example 2:
+
+
+
+Input: startTime = [1,2,3,4,6], endTime = [3,5,10,6,9], profit = [20,20,100,70,60]
+Output: 150
+Explanation: The subset chosen is the first, fourth and fifth job. 
+Profit obtained 150 = 20 + 70 + 60.
+Example 3:
+
+
+
+Input: startTime = [1,1,1], endTime = [2,3,4], profit = [5,6,4]
+Output: 6
+ 
+
+Constraints:
+
+1 <= startTime.length == endTime.length == profit.length <= 5 * 104
+1 <= startTime[i] < endTime[i] <= 109
+1 <= profit[i] <= 104
+*/
+class Solution {
+    private class Job{
+        int startTime;
+        int endTime;
+        int profit;
+        private Job(){}
+        private Job(int sTime, int eTime, int profit){
+            this.startTime = sTime;
+            this.endTime = eTime;
+            this.profit = profit;
+        }
+    }
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        List <Job> jobs = new ArrayList<>();
+        for(int i = 0; i < startTime.length; i++){
+            jobs.add(new Job(startTime[i], endTime[i], profit[i]));
+        }
+        Collections.sort(jobs, (a, b) -> a.endTime - b.endTime);
+        
+        TreeMap <Integer, Integer> map = new TreeMap<>();
+        int ans = 0;
+        for(Job currJob : jobs){
+            Integer entryTillStartTime = map.floorKey(currJob.startTime);
+            int maxProfitTillCurrTime = (entryTillStartTime == null) ? 0 : map.get(entryTillStartTime);
+            ans = Math.max(ans,maxProfitTillCurrTime  + currJob.profit);
+            map.put(currJob.endTime, ans);            
+        }
+        return ans;
+        
+    }
+}
